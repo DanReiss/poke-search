c928a3bc81822a580619e40ffc2d2183b28787f5
Danreiss <danreis.ctt@gmail.com>
2022-12-09 08:50:13 -0300

    carregando, falta o search

--- script.js
+++ script.js
@@ -1,19 +1,63 @@
-document.addEventListener("DOMContentLoaded", LoadPokemons)
+// document.addEventListener("DOMContentLoaded", fetchPokemons)
 
-function LoadPokemons(){
-    let innerRow = "";
-    for(let i = 1, r = 0; i <= 16; i++){
-        if(i % 4 === 1){
-            innerRow = ""; // resetar para começar uma nova linha
-            r++;
-        }
-        let row = document.querySelector(`.row${r}`)    
-        let pokemonBox = `<div class="col">
-                <h4>Name</h4>
-                <img src="" alt="img pokemon">
-                <p>ID Pokemon</p>
-                </div>`;
-        innerRow += pokemonBox;
-        row.innerHTML = innerRow;
+let pokeContainer = document.querySelector(".pokemons-container")
+
+
+    // if(i % 4 === 1){
+    //     innerRow += `<div class="row row${row}">`
+    // }
+    // innerRow += `<div class="col card m-2">
+    //                 <h4>${pokemon.name}</h4>
+    //                 <img src="${pokemon.sprites['front_default']}" alt="image ${pokemon.name}">
+    //                 <p>${format(pokemon.id)}</p>
+    //             </div>`;
+    // if(i % 4 === 0){
+    //     innerRow += "</div>"
+    // }    
+    
+
+
+const fetchPokemons = () =>{
+    const getURL = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+
+    const pokemonPromises = [];
+
+    for(let i =1; i<= 20; i++){
+        pokemonPromises.push(fetch(getURL(i)).then(response => response.json()))
     }
+
+    Promise.all(pokemonPromises)
+    .then(pokemonsData =>{
+        loadPokemons(pokemonsData)
+    })
 }
+
+fetchPokemons();
+
+
+function loadPokemons(pokemons){
+    const innerRows = pokemons.reduce((acc, pokemon, index) => {
+        let row = 1;
+        if((index + 1) % 4 === 1){
+            acc += `<div class="row row${row}">`
+        }
+        acc += `<div class="col card m-2">
+                        <h4>${pokemon.name}</h4>
+                        <img src="${pokemon.sprites['front_default']}" alt="image ${pokemon.name}">
+                        <p>${format(pokemon.id)}</p>
+                    </div>`;
+        if((index + 1) % 4 === 0){
+            acc += "</div>"
+            row++
+        } 
+        return acc
+    }, "");
+    pokeContainer.innerHTML = innerRows;
+}
+
+function format(id){
+    if(id < 10) return "Nº00" + id;
+    if(id < 100 && id >= 10) return "Nº0" + id;
+    if(id > 100) return "Nº" + id;
+}
+
